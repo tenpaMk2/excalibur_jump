e97264036ecd641b49f1faa79fa79b22a00132b3
tenpaMk2 <wavyroom@gmail.com>
2021-10-01 22:48:36 +0900

    :fire: remove unused codes.

--- src/level.ts
+++ src/level.ts
@@ -64,36 +64,5 @@ export class Level extends Scene {
     // });
 
     platforms.forEach((platform) => engine.add(platform));
-
-    // const hoge = new Actor({
-    //   pos: new Vector(engine.halfDrawWidth, engine.halfDrawHeight),
-    //   width: engine.halfDrawWidth,
-    //   height: engine.halfDrawHeight,
-    //   color: Color.Chartreuse,
-    // });
-    // engine.add(hoge);
-    // hoge.onPreUpdate = () => {
-    //   hoge.width = engine.halfCanvasWidth;
-    // };
-
-    // const fuga = new UIActor({
-    //   x: engine.drawWidth * 0.1,
-    //   y: engine.drawHeight * 0.1,
-    //   width: 10,
-    //   height: 10,
-    //   color: Color.Black,
-    // });
-    // engine.add(fuga);
-
-    // const piyo = new Actor({
-    //   pos: Vector.Zero,
-    //   anchor: Vector.Zero,
-    //   width: 200,
-    //   height: 5,
-    //   color: Color.Blue,
-    // });
-    // fuga.add(piyo);
-
-    // engine.currentScene.camera.zoom(2, 1000);
   };
 }
